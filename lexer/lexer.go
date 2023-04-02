c65b9fbf41565abe568a8ef1273d683f14f25da5
Mateusz Różański <rozanski.mateusz00@gmail.com>
2023-04-02 03:32:55 +0200

    Added handling numbers in lexer, now more complex tests pass

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -64,6 +64,10 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdentifier(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
 		} else {
 			tok = token.New(token.ILLEGAL, l.ch)
 		}
@@ -91,4 +95,17 @@ func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
 	}
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
 }
